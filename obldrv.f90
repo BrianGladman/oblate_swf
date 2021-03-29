36536cdbc1f1cf76c601aa9746d7dbd51706e787
Brian Gladman <riemannic@gmail.com>
2021-03-29 17:03:44 +0100

    initial commit

---
+++ obldrv.f90
@@ -0,0 +1,96 @@
+ program obldrv
+    use param
+    use oblate_swf
+    
+    implicit none
+    
+    integer   i, im, j, mmin, minc, mnum, m, l, lnum, ioprad, iopang, &
+              iopnorm, ioparg, narg, kind, kindd, kindq
+    real(knd) c, x, x1, arg, arg1, darg, pi, api
+
+    real(knd), dimension(:), allocatable ::    eta, r1c, r1dc, r2c, r2dc
+    integer, dimension(:), allocatable ::      ir1e, ir1de, ir2e, ir2de, naccr
+    real(knd), dimension (:,:), allocatable :: s1c, s1dc
+    integer, dimension(:,:), allocatable ::    is1e, is1de, naccs
+
+    kindd =  8
+    kindq = 16
+
+!
+!  open input files
+        open(1, file='oblfcn.dat')
+!
+!  read input data
+        read(1,*) mmin,minc,mnum,lnum
+        read(1,*) ioprad,iopang,iopnorm
+        read(1,*) c,x
+        if(iopang.ne.0) read(1,*) ioparg,arg1,darg,narg
+
+    allocate (eta(narg), r1c(lnum), r1dc(lnum), r2c(lnum), r2dc(lnum))
+    allocate (ir1e(lnum), ir1de(lnum), ir2e(lnum), ir2de(lnum), naccr(lnum))
+    allocate (s1c(lnum, narg), s1dc(lnum, narg))
+    allocate (is1e(lnum, narg), is1de(lnum, narg), naccs(lnum, narg))
+
+    pi = acos(-1.0_knd)
+    api = pi / 180.0e0_knd
+    if (iopang /= 0) then
+        do j = 1, narg  
+            eta(j) = cos(api * (arg1 + (j - 1) * darg))        
+        end do
+    end if
+
+    do im = 1, mnum
+        m = mmin + (im - 1) * minc
+
+            call oblfcn(c, m, lnum, ioprad, x, iopang, iopnorm, narg, eta, &
+                        r1c, ir1e, r1dc, ir1de, r2c, &
+                        ir2e, r2dc, ir2de, naccr, &
+                        s1c, is1e, s1dc, is1de, naccs)
+        
+        if (ioprad /= 0) then
+
+            if(knd == kindd) write(20, 10) x, c, m
+            if(knd == kindq) write(20, 20) x, c, m
+10          format(1x,e23.14,e23.14,i5)
+20          format(1x,e40.30,e40.30,i5)
+            
+            do i = 1, lnum
+                l = m + i - 1
+                if(ioprad == 2) write(20, 30) l, r1c(i), ir1e(i), r1dc(i), ir1de(i), r2c(i), ir2e(i), r2dc(i), ir2de(i), naccr(i)
+                if(ioprad == 1) write(20, 40) l, r1c(i), ir1e(i), r1dc(i), ir1de(i)
+30              format(1x,i5,2x,4(f17.14,i6,2x),i2, ' ')
+40              format(1x,i5,2x,2(f17.14,i6,2x),i2)
+            end do
+            
+        end if
+
+        if (iopang /= 0) then
+            
+            if(knd == kindd) write(30, 50) c, m
+            if(knd == kindq) write(30, 60) c, m
+50          format(1x,e23.14,i5)
+60          format(1x,e40.30,i5)
+            
+            do i = 1, lnum
+                l = m + i - 1
+
+                write(30, 70) l
+70              format(1x,i6)
+
+                do j = 1, narg
+                    arg = arg1 + (j - 1) * darg
+                    write(30, 80) arg, s1c(i, j), is1e(i, j), s1dc(i, j), is1de(i, j), naccs(i, j)
+80                  format(1x,f19.14,2x,f17.14,2x,i5,2x,f17.14,2x,i5,2x,i2)
+90                  format(1x,f17.14,2x,f17.14,2x,i5,2x,f17.14,2x,i5,2x,i2,', ',i2)
+                end do
+            end do
+        end if
+    end do
+    
+    deallocate (is1e, is1de, naccs)
+    deallocate (s1c, s1dc)
+    deallocate (ir1e, ir1de, ir2e, ir2de, naccr)
+    deallocate (eta, r1c, r1dc, r2c, r2dc)
+    close(1)
+
+end program obldrv
